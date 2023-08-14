46cbb683dfea367d5c6a39122aee9c7515617f82
Ozioma7 <okonisaozioma@gmail.com>
2023-08-14 21:59:19 +0000

    task four

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-    for i in my_list:
-        my_list[idx] = element
-    print (my_list)
+    dup = [x for x in my_list]
+    dup[idx] = element
+    return dup
