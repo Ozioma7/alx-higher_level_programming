af33d72f5ae6dd5a2d72cdf07a1c00598013be93
Ozioma7 <okonisaozioma@gmail.com>
2023-08-29 18:42:58 +0000

    task three

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,11 +10,11 @@ class Square:
             size (int):
         """
 
-    if type(size) is not int:
-        raise TypeError('size must be an integer')
-    elif type(size) < 0:
-        raise ValueError('size must be >= 0')
-    self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif type(size) < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     def area(self):
         return (self.__size ** 2)
