86724d968970a17e330fbae80eaf10ac0bc5e7b9
Ozioma7 <okonisaozioma@gmail.com>
2023-09-06 23:48:11 +0000

    task six

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,8 +4,12 @@
 
 class Rectangle:
     """Representing a Rectangle"""
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialization"""
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
