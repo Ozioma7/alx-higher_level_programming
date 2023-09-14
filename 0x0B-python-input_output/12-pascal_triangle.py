cacd2b60fe4b6561d6f339bb4634385ba4237811
Ozioma7 <okonisaozioma@gmail.com>
2023-09-14 01:29:45 +0000

    amends

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
         print(list)
         newlist = []
         newlist.append(list[0])
-        for i in range(len(list) -1):
+        for i in range(len(list) - 1):
             newlist.append(list[i] + list[i + 1])
         newlist.append(list[-1])
         list = newlist
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Adding Items"""
+"""Add all arguments"""
 import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').
-    load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-    args = sys.argv
-    filename = "add_item.json"
-    with open(filename, 'a+', encoding="utf-8") as f:
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
         items = []
-        my_list.append(args[1:])
-        save_to_json_file(items, filename)
-        load_from_json_file(filename)
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
