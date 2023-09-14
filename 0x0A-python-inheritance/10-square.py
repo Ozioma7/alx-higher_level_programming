be88adcf2125e73c1e034f6ac78d69ac4186b187
Ozioma7 <okonisaozioma@gmail.com>
2023-09-14 01:09:22 +0000

    corrections

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Intialization"""
-	self.integer_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Intialization"""
-	super().integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
         self.__height = height
