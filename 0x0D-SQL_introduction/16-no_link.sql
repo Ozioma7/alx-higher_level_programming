eb22ed4c095ee7b266a3e32a5a68f38fbc30cc99
Ozioma7 <okonisaozioma@gmail.com>
2023-10-17 18:26:56 +0000

    tasks 13-16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Lcript that lists all records of the table second_table
+-- Don’t list rows without a name value
+
+SELECT score, name
+FROM second_table
+HAVING name IS NOT NULL
+ORDER BY score DESC;
