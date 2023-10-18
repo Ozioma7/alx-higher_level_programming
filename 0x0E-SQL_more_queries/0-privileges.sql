0149a7385a86afab6365750c072bef3acf988659
Ozioma7 <okonisaozioma@gmail.com>
2023-10-18 16:23:32 +0000

    tasks 0 & 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Llists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
 
-SHOW GRANTS FOR 'user_0d_1' AND 'user_od_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
